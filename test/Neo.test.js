502dfc7ded1c2d19df96c893a8693a38b6db1d4e
Tom Van Schoor <tom@tutuka.com>
2019-02-06 15:37:51 +0100

    Allow for a model to be added to Neo

--- test/Neo.test.js
+++ test/Neo.test.js
@@ -5,6 +5,7 @@ const { Driver } = require('neo4j-driver/lib/v1/driver')
 const { srcRequire } = require('./utils')
 
 const Neo = srcRequire('Neo')
+const Model = srcRequire('Model')
 
 describe('::Neo', () => {
   let neo = null
@@ -18,6 +19,20 @@ describe('::Neo', () => {
       expect(neo.models).to.be.an.instanceOf(Map)
       expect(neo.models.size).to.eq(0)
       expect(neo.driver).to.be.an.instanceOf(Driver)
+    })
+  })
+
+  describe(':model', () => {
+    const name = 'TestModel'
+    const schema = {}
+    it('should create, add and return a new model', () => {
+      const model = neo.model(name, schema)
+      expect(model).to.be.instanceOf(Model)
+    })
+
+    it('should return an existing model', () => {
+      const model = neo.model(name, schema)
+      expect(neo.model(name)).to.eq(model)
     })
   })
 })
